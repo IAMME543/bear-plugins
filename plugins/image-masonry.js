2f2e1c6ed24d7417050a23012d4186407cb92f6e
Mason <152034114+IAMME543@users.noreply.github.com>
2026-03-18 17:30:53 +1030

    Am blind. moved script to before we need it instead of after

--- plugins/image-masonry.js
+++ plugins/image-masonry.js
@@ -8,7 +8,7 @@
 /* USAGE
 It is recomended to manually compress images before storing on github as it makes everything load faster and reduces bandwidth usage.
 
-Change the first 4 values (owner, repo, folder, columnCount) or use data attributes in the src declaration as shown here:
+Change the 4 values (owner, repo, folder, columnCount) or use data attributes in the src declaration as shown here:
 
 > <src data-owner="Github username" data-repo="Github repository name" data-folder="Repository folder name" data-columns="3"></src> 
 
@@ -16,7 +16,8 @@ Change the first 4 values (owner, repo, folder, columnCount) or use data attribu
 
 (function () {
     'use strict';
-
+    const script = document.currentScript;
+ 
      // Set as github username
      const owner = script.dataset.owner;
      // Set as the repository containing images
@@ -61,10 +62,6 @@ Change the first 4 values (owner, repo, folder, columnCount) or use data attribu
 }`;
 
     document.head.appendChild(style);
-
-    const script = document.currentScript;
-
-
 
     const columns = [];
 
